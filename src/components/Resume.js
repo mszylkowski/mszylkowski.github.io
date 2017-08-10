c7448980cb6f096db3c576c42c07f43e81e1d6c6
Matias Szylkowski <mszylkowski@hotmail.com>
2017-08-10 01:49:58 -0300

    update resume source

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ export default class Bio extends Component {
     return (
       <div className="container container-bio">
         <div className="container">
-          <a className="button button-yellow" href="https://drive.google.com/file/d/0B7P-6hSsgGpYYk5jMDBPMlU5OEE/view" download="MatiasSzylkowski_Resume">Download as PDF<i className="fa fa-cloud-download"></i></a>
+          <a className="button button-yellow" href="https://drive.google.com/file/d/0B7P-6hSsgGpYYk5jMDBPMlU5OEE/view" download="MatiasSzylkowskiResume">Download as PDF<i className="fa fa-cloud-download"></i></a>
           <a className="button button-yellow" href="https://drive.google.com/file/d/0B7P-6hSsgGpYYk5jMDBPMlU5OEE/view" target="_blank">View as PDF<i className="fa fa-arrow-circle-right"></i></a>
         </div>
         <div className="container-center container-dark animated bounceInUp" style={{padding: '0px'}}>
